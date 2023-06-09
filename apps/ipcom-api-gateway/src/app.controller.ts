dbbddf0eb37e28c9277838c58cef738064c4c8d8
Alexander Daza <dev.alexander.daza@gmail.com>
2023-06-08 23:55:03 -0500

    chore: finished get stadistics

--- apps/ipcom-api-gateway/src/app.controller.ts
+++ apps/ipcom-api-gateway/src/app.controller.ts
@@ -2,6 +2,9 @@ import { ResumeDateDto, ResumeDaysDto, SaleDto } from '@ipcom/shared';
 
 import { Controller, Get, Param, Query } from '@nestjs/common';
 
+import { Decimal } from 'decimal.js';
+
+import { SalesStadisticsDto } from './app.dto';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,18 +21,76 @@ export class AppController {
   async getSales(
     @Param() params: ResumeDateDto,
     @Query() query: ResumeDaysDto,
-  ): Promise<SaleDto[]> {
+  ): Promise<SalesStadisticsDto> {
+    // Get days to get sales
     const days: number = query.dias ? query.dias : 1;
+
+    // Initialize sales
     const sales: SaleDto[] = [];
+
+    // Iterate over days
     for (let i = 0; i < days; i++) {
+      // Transform date
       const date: Date = new Date(params.date);
+
+      // Add days to date
       date.setDate(date.getDate() + i);
+
+      // Get sales from microservice and add to sales
       sales.push(
         ...(await this.appService.getSales({
           date: date.toISOString(),
         })),
       );
     }
-    return sales;
+
+    // Return stadistics
+    return await this.getStadistics(sales);
+  }
+
+  /**
+   * @description Get stadistics from sales
+   * @param sales Sales to get stadistics
+   * @returns Stadistics of sales
+   */
+  private async getStadistics(sales: SaleDto[]): Promise<SalesStadisticsDto> {
+    // Initialize variables
+    let totalSales = new Decimal(0);
+    let maxSale = new Decimal(0);
+    let notBuyers = 0;
+    const totalSalesByCreditCard: Record<string, Decimal> = {};
+
+    // Iterate over sales
+    for (const sale of sales) {
+      if (sale.compro) {
+        // Sum all sales
+        totalSales = totalSales.plus(sale.monto);
+        // Check for max sale
+        if (sale.monto && new Decimal(sale.monto).greaterThan(maxSale)) {
+          maxSale = new Decimal(sale.monto);
+        }
+        // Count all buyers by credit card
+        if (sale.tdc) {
+          if (totalSalesByCreditCard[sale.tdc]) {
+            totalSalesByCreditCard[sale.tdc] = totalSalesByCreditCard[
+              sale.tdc
+            ].plus(sale.monto);
+          } else {
+            totalSalesByCreditCard[sale.tdc] = new Decimal(sale.monto);
+          }
+        }
+      } else {
+        // Count all not buyers
+        notBuyers++;
+      }
+    }
+
+    // Return stadistics
+    return {
+      total: totalSales.toNumber(),
+      comprasPorTDC: totalSalesByCreditCard,
+      nocompraron: notBuyers,
+      compraMasAlta: maxSale,
+    };
   }
 }
